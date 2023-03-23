77378b44796f1be72cd7ca666c40be3a49725434
Fernando <fermx3@gmail.com>
2023-03-23 15:01:37 -0600

    Add data file

---
+++ data/menu.js
@@ -0,0 +1,144 @@
+MENU_DB = [
+  {
+    categoria: 'Entradas',
+    caegory: 'Appetizers',
+    platillos: [
+      {
+        nombre: 'Guacamole',
+        desc: 'Aguacate, cebolla, tomate, cilantro, lechuga y queso fresco mezclado para crear una delicia mexicana.',
+        descEng:
+          'Avocado, onion, tomato, coriander, lettuce and cheese mixed together to create a traditional mexican dish.',
+        precio: 0,
+      },
+      {
+        nombre: 'Nachos',
+        desc: 'Totopos crujientes y calientes acompañados de queso gouda, queso cheddar, jalapeño, pico de gallo y frijoles.',
+        descEng:
+          'Crunchy and hot tortilla chips with gouda cheese, cheddar cheese, jalapeños, pico de gallo and beans.',
+        variantes: [
+          {
+            nombre: 'Pollo',
+            name: 'Chicken',
+            precio: 200,
+          },
+          {
+            nombre: 'Arrechera',
+            name: 'Flank Steak',
+            precio: 250,
+          },
+          {
+            nombre: 'Mixto',
+            name: 'Mixed',
+            desc: 'Pollo, Arrachera, Chuleta',
+            descEng: 'Chicken, Flank Steak, Pork Chop',
+            precio: 200,
+          },
+        ],
+      },
+      {
+        nombre: 'Queso Fundido',
+        name: 'Melted Cheese',
+        variantes: [
+          {
+            nombre: 'Natural',
+            name: 'Plain',
+            precio: 199,
+          },
+          {
+            nombre: 'Pollo',
+            name: 'Chicken',
+            precio: 230,
+          },
+          {
+            nombre: 'Arrachera',
+            name: 'Flank Steak',
+            precio: 200,
+          },
+          {
+            nombre: 'Chuleta',
+            name: 'Pork Chop',
+            precio: 220,
+          },
+          {
+            nombre: 'Vegetales',
+            name: 'Vegetables',
+            precio: 220,
+          },
+        ],
+      },
+      {
+        nombre: 'Aguachile',
+        desc: 'Camarones, chiles frescos, limón, tiras de cebolla y tiras de pepino.',
+        descEng:
+          'Shrimp, fresh chillies, lemon, onion strips and cucumber strips.',
+        precio: 0,
+      },
+      {
+        nombre: 'Aros de Calamar',
+        name: 'Squid Rings',
+        desc: 'Crujientes aros de calamar fritos y servidos con salsa tártara hecha en casa.',
+        descEng: 'Crunchy squid rings served with our home made tartar sauce.',
+        precio: 0,
+      },
+      {
+        nombre: 'Cóctel de Camarón',
+        name: 'Shrimp Cocktail',
+        desc: 'Camarones servidos con nuestra sabrosa salsa coctelera, cebolla, cilantro y aguacate.\nServido con totopos o galletas saladas.',
+        descEng:
+          'Shrimp served with our cocktail sauce, onion, coriander and avocado.\nServed with tortilla chips or salty crackers.',
+        precio: 0,
+      },
+      {
+        nombre: 'Ceviche de Camarón',
+        name: 'Shrimp Ceviche',
+        desc: 'Camarones marinados con jugo de limón y pico de gallo.\nServido con totopos o galletas saladas.',
+        descEng:
+          'Shrimp ceviche marinated with lemon juice and pico de gallo.\nServed with tortilla chips or salty crackers.',
+        precio: 0,
+      },
+    ],
+  },
+  {
+    categoria: 'Ensaladas',
+    category: 'Salads',
+    platillos: [
+      {
+        nombre: 'Mixta',
+        desc: 'Lechuga, tomate, pimiento verde, cebolla, pepino, zanahoria y aguacate.\nAcompañado de aderezo cesar o ranch.',
+        descEng:
+          'Lettuce, tomato, green pepper, onion, cucumber, carrot and avocado.\nWith cesar or ranch dressing.',
+      },
+      {
+        nombre: 'César',
+        desc: 'Ensalada de lechuga romana y croutons(trozos de pan tostado) y aderezo cesar.',
+        descEng:
+          'Salad of romaine lettuce and croutons(pieces of toasted bread) and caesar dressing.',
+      },
+      {
+        nombre: 'Caprese',
+        desc: 'Rodajas de tomate y de mozzarella fresca, hojas de albahaca fresca, bañadas con aceite de oliva.',
+        descEng:
+          'Tomato slices and fresh mozzarella, fresh basil leaves, drizzled with olive oil.',
+      },
+    ],
+  },
+  {
+    categoria: 'Sopas',
+    category: 'Soups',
+    platillos: [
+      {
+        nombre: 'Sopa de Lima',
+        name: 'Lime Soup',
+        desc: 'Un caldo ligero de pollo, acompañado de verduras, pollo desmenuzado, lima y tiras de tortilla frita.',
+        descEng:
+          'A light chicken broth, with vegetables, shredded chicken, lime and fried tortilla strips.',
+      },
+      {
+        nombre: 'Cazuela de Mariscos',
+        desc: 'Pescado, jaiba, almeja, camarón, mejillón, calamar y queso gouda derretido.',
+        descEng:
+          'Fish, blue crab, clam, shrimp, mussel, squid and melted gouda cheese.',
+      },
+    ],
+  },
+];
